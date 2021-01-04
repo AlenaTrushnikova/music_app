180fbef14ead48fa9db697aa4ff600f75a782244
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-01-04 15:20:58 -0800

    clean up

--- app/controllers/songs/songs_in_playlists_controller.rb
+++
@@ -1,9 +0,0 @@
-class Songs::SongsInPlaylistsController < SongsInPlaylistsController
-    before_action :set_playable
-
-    private 
-
-    def set_playable
-        @playable = Song.find(params[:song_id])
-    end
-end
