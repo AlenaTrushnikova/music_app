cd4469f42625f366fecf2fe53d3d386b0f28ede1
Alena Trushnikova <alena.yu.t@gmail.com>
2021-01-05 15:34:05 -0800

    bootstrap for body

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     validates :email, uniqueness: true, presence: true
     validates :username, uniqueness: true, presence: true
     has_secure_password
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
 end
